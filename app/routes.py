c26d8c8c942c23d5cffcae2652744c13845de644
Aditya Karun <114733642+AdityaKarun@users.noreply.github.com>
2026-03-03 22:13:50 +0530

    add ip-details route to the application

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+from .services.ip_service import get_data_from_ip
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route("/ip-details")
+def get_ip_details():
+    x_forwarded = request.headers.get("X-Forwarded-For")
+
+    if x_forwarded:
+        ip = x_forwarded.split(',')[0].strip()
+    else:
+        ip = request.remote_addr
+
+    data = get_data_from_ip(ip)
+    return jsonify(data)
