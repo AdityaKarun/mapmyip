46c7b0d7a86587c66eba33a9f4f95af9fa8d3459
Aditya Karun <114733642+AdityaKarun@users.noreply.github.com>
2026-04-14 22:33:02 +0530

    Setup PostgreSQL integration with Flask-SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,32 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
 from .routes import main
+
+db = SQLAlchemy()
 
 def create_app():
     """
-    Application factory — creates and configures the Flask app.
-    Using a factory function (rather than a module-level app instance)
-    makes it easier to run multiple configs e.g. testing vs production.
+    Application factory — builds and configures the Flask app instance.
+
+    - Loads environment variables from `.env`
+    - Sets up database configuration and binds SQLAlchemy to the app
+    - Registers blueprints (routes)
+
+    Using a factory function avoids global app state and allows
+    flexible configurations (e.g., development, testing, production).
     """
+    load_dotenv()
+
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     app.register_blueprint(main)
 
